8de49f66dc9df72fa97569ce020acacae7130b96
Rohit Ragesh <rohitrag71@gmail.com>
2023-01-08 19:17:03 -0800

    Finished Day 1

---
+++ day_1.py
@@ -0,0 +1,28 @@
+Day1=open('Day 1.txt','r')
+
+content=Day1.readlines()
+
+p=''
+for i in content:
+  p=p+str(i)
+
+list1=[]
+for i in content :
+    list1=list1+[i.strip()]
+
+list1
+
+sums=[]
+sum=0
+for number in list1:
+  if number=='':
+    sums.append(sum)
+    sum=0
+  else: sum+= int(number)
+print(sums)
+
+sums=sorted(sums)
+
+sums
+
+print("%d, %d, %d" %(sums[-1],sums[-2],sums[-3]))
